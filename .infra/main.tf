30a76658e54084c712d0f29d637aa4dccdbb728c
Adam Stoffel <2475873+adamstoffel@users.noreply.github.com>
2021-05-16 17:33:59 -0700

    Fix deployment (#4)
    * Disable always on which isn't supported in Free functions SKU
    
    * Upgrade app service plan SKU
    
    * Always run plan
    
    * Functions app HTTPS only

--- .infra/main.tf
+++ .infra/main.tf
@@ -52,8 +52,8 @@ resource "azurerm_app_service_plan" "main_plan" {
   reserved            = true
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
@@ -73,6 +73,7 @@ resource "azurerm_function_app" "backend_api" {
   storage_account_name       = azurerm_storage_account.main_storage.name
   storage_account_access_key = azurerm_storage_account.main_storage.primary_access_key
   os_type                    = "linux"
+  https_only                 = true
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE"       = "1",
     "FUNCTIONS_WORKER_RUNTIME"       = "node",
@@ -80,8 +81,8 @@ resource "azurerm_function_app" "backend_api" {
     "StorageAccountConnectionString" = azurerm_storage_account.main_storage.primary_connection_string
   }
   site_config {
-    linux_fx_version          = "node|lts"
-    use_32_bit_worker_process = true
+    always_on        = true
+    linux_fx_version = "node|lts"
     cors {
       allowed_origins = [azurerm_app_service.cat_game.default_site_hostname]
     }
