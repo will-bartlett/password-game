7f7f708e98d8dbdce17d3f2c96ad8c6265938b45
Adam Stoffel <2475873+adamstoffel@users.noreply.github.com>
2021-05-19 17:44:16 -0700

    Initial avatar bits

---
+++ cat-game/src/assets/avatars/avatars.ts
@@ -0,0 +1,12 @@
+const avatarList = {
+    "cat1": "logo.png",
+    "cat2": "logo.png",
+    "cat3": "logo.png",
+    "cat4": "logo.png",
+    "cat5": "logo.png",
+    "cat6": "logo.png",
+    "cat7": "logo.png",
+    "cat8": "logo.png",
+};
+
+export default avatarList;
